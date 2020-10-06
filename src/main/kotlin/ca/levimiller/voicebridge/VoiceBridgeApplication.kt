fd1c25b4889e18340f615ab9811e3cef203482cd
levimiller <levi@levimiller.ca>
2020-10-05 19:29:01 -0700

    Add initial project

---
+++ src/main/kotlin/ca/levimiller/voicebridge/VoiceBridgeApplication.kt
@@ -0,0 +1,11 @@
+package ca.levimiller.voicebridge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoiceBridgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoiceBridgeApplication>(*args);
+}
---
+++ src/test/kotlin/ca/levimiller/voicebridge/VoiceBridgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ca.levimiller.voicebridge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoiceBridgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
